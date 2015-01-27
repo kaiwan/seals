f80fea9a0a3b9847f763a537359c3058e7e9b93c
kaiwan n billimoria <kaiwan@kaiwantech.com>
2015-01-27 07:38:16 +0530

    Minor fix

--- run-direct.sh
+++ run-direct.sh
@@ -10,4 +10,4 @@
  exit 1
 }
 KERN=$1
-qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN}/arch/arm/boot/zImage -drive file=.staging/images/rfs.img,if=sd -append "console=ttyAMA0 root=/dev/mmcblk0 init=/sbin/init" -nographic
+qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN}/arch/arm/boot/zImage -drive file=./staging/images/rfs.img,if=sd -append "console=ttyAMA0 root=/dev/mmcblk0 init=/sbin/init" -nographic
