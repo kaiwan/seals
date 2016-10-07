050ad568f879dcef5aa00b973735c65267d68399
Kaiwan N Billimoria <kaiwan@kaiwantech.com>
2016-10-07 08:50:33 +0530

    Small cleanup

--- run-direct.sh
+++ run-direct.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 # Part of the SEALs project
 # (c) kaiwanTECH
-[ $# -ne 1 ] && {
- echo "Usage: $0 path-to-kernel-srctree for ARM/Linux kernel"
- exit 1
-}
-[ ! -d $1 ] && {
- echo "$0: kernel path $1 invalid? Aborting..."
- exit 1
-}
-KERN=$1
-qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN}/arch/arm/boot/zImage -drive file=./staging/images/rfs.img,if=sd -append "console=ttyAMA0 root=/dev/mmcblk0 init=/sbin/init" -nographic
+STG_IMG=~/big/scratchpad/SEALS_staging/images
+KERN=${STG_IMG}/zImage
+ROOTFS=${STG_IMG}/rfs.img
+[ $# -eq 1 ] && KERN=$1
+
+qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN} -drive file=${ROOTFS},if=sd,format=raw -append "console=ttyAMA0 root=/dev/mmcblk0 init=/sbin/init" -nographic
