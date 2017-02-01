63f956384fd8c7b0dccdcbde9b7da90ef9169696
Kaiwan N Billimoria <kaiwan@kaiwantech.com>
2017-02-01 09:20:13 +0530

    add debug param

--- run-direct.sh
+++ run-direct.sh
@@ -8,7 +8,7 @@ ROOTFS=${STG_IMG}/rfs.img
 
 #K_CMDLINE="console=ttyAMA0 rootfstype=ext4 root=/dev/mmcblk0 init=/sbin/init initcall_debug ignore_loglevel"
 K_CMDLINE_BASE="console=ttyAMA0 rootfstype=ext4 root=/dev/mmcblk0 init=/sbin/init"
-K_CMDLINE_XTRA="initcall_debug ignore_loglevel"
+K_CMDLINE_XTRA="initcall_debug ignore_loglevel debug"
 K_CMDLINE="${K_CMDLINE_BASE} ${K_CMDLINE_XTRA}"
 
 qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN} \
