0a015a961e93d4e7b837987b0d818e94c74d3319
Kaiwan N Billimoria <kaiwan@kaiwantech.com>
2017-03-24 09:14:20 +0530

    Include DTB when running

--- run-direct.sh
+++ run-direct.sh
@@ -5,13 +5,19 @@ STG_IMG=~/big/scratchpad/SEALS_staging/images
 KERN=${STG_IMG}/zImage
 ROOTFS=${STG_IMG}/rfs.img
 [ $# -eq 1 ] && KERN=$1
+DTB=${STG_IMG}/vexpress-v2p-ca9.dtb
 
 #K_CMDLINE="console=ttyAMA0 rootfstype=ext4 root=/dev/mmcblk0 init=/sbin/init initcall_debug ignore_loglevel"
 K_CMDLINE_BASE="console=ttyAMA0 rootfstype=ext4 root=/dev/mmcblk0 init=/sbin/init"
 K_CMDLINE_XTRA="initcall_debug ignore_loglevel debug"
 K_CMDLINE="${K_CMDLINE_BASE} ${K_CMDLINE_XTRA}"
 
-qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN} \
+RUNCMD="qemu-system-arm -m 256 -M vexpress-a9 -kernel ${KERN} \
 	-drive file=${ROOTFS},if=sd,format=raw \
-	-append "${K_CMDLINE}" \
-	-nographic
+	-append \"${K_CMDLINE}\" \
+	-nographic"
+[ -f ${DTB} ] && RUNCMD="${RUNCMD} -dtb ${DTB}"
+echo
+echo "${RUNCMD}"
+echo
+eval ${RUNCMD}
