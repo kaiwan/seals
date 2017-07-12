6f7a58dcdb0cbca2fe4b87b55cf1f14fb61af3b0
Kaiwan N Billimoria <kaiwan@kaiwantech.com>
2017-07-12 15:48:31 +0530

    avoid check-for-root-user, use sudo as required instead [cleaner]

--- mount_rootfs.sh
+++ mount_rootfs.sh
@@ -16,37 +16,32 @@ IMG=/mnt/big/scratchpad/SEALS_staging/images/rfs.img  # @@@@@ Update as required
 MNTPT=/mnt/tmp
 
 name=$(basename $0)
-[ $(id -u) -ne 0 ] && {
- echo "${name}: need to be root."
- exit 1
-}
 [ ! -f ${IMG} ] && {
- echo "${name}: need to be root."
+ echo "${name}: root filesystem image file \"${IMG}\" unavailable, pl fix & rerun."
  exit 1
 }
-
 echo "${name}: Rootfs image file: ${IMG}"
 echo "${name}: Please wait... checking if rootfs image file above is currently in use ..."
-lsof 2>/dev/null |grep ${IMG} && {
+sudo lsof 2>/dev/null |grep ${IMG} && {
   echo "${name}: Rootfs image file \"${IMG}\" currently in use, aborting..." 
   echo " Is it being used by a QEMU instance perhaps? If so, shut it down and retry this."
   exit 1
 }
 
 echo "${name}: Okay, loop mounting rootfs image file now ..."
-mount |grep -iq ${MNTPT} && {
+sudo mount |grep -iq ${MNTPT} && {
   sync
-  umount ${MNTPT}
+  sudo umount ${MNTPT}
 }
-mkdir -p ${MNTPT} 2>/dev/null
-mount -o loop -t ext4 ${IMG} ${MNTPT} && {
+sudo mkdir -p ${MNTPT} 2>/dev/null
+sudo mount -o loop -t ext4 ${IMG} ${MNTPT} && {
  echo "${IMG} loop mounted at ${MNTPT}"
  mount |grep "${IMG}"
  echo
  echo "Update fs contents, then remember you MUST umount it ..."
  echo
  echo "ls ${MNTPT} :"
- ls ${MNTPT}
+ sudo ls ${MNTPT}
 } || {
  echo "${name}: ${IMG} loop mounting failed! aborting..."
  exit 1
